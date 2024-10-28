a1689f85f8c4cb228f1cacf688e8c5a8d3b159aa
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-10-28 16:57:19 +0000

    Update example.ts

--- example.ts
+++ example.ts
@@ -1,22 +1,36 @@
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { environment, PaginatedResponse } from '@env/environment'
+
 export class Content {
-    id: string
-    title: string
-    type: string
-    description: string
-    modelName: string
-    urls: string[]
-    imagePreview: string
-    infringementPreferences: InfringementPreferences
-    files: string[]
-    detectionStrings: string[]
-    allowedDomains: string[]
-    is_verified: boolean
-    owner: string
-  }
+  id: string
+  title: string
+  type: string
+  description: string
+  modelName: string
+  urls: string[]
+  imagePreview: string
+  infringementPreferences: InfringementPreferences
+  files: string[]
+  detectionStrings: string[]
+  allowedDomains: string[]
+  is_verified: boolean
+  owner: string
+}
   
 
-  export interface InfringementPreferences {
-    
-  }
+export interface InfringementPreferences {
+  
+}
   
   
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentService {
+  private apiUrl = `${environment.APIHost}/api/client/contents`
+
+  constructor (private http: HttpClient) { }
+
+}
